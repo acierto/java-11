594fdb591f332a123a7fc19850d10f7b0c429fbd
acierto <bogdannechiporenko@gmail.com>
2020-11-05 12:31:13 +0100

    strings example

---
+++ src/main/java/com/acierto/java11/strings/StringsExample.java
@@ -0,0 +1,13 @@
+package com.acierto.java11.strings;
+
+public class StringsExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("a").append("b").append("c");
+        System.out.println(sb.reverse());
+
+        String str1 = "string";
+        System.out.println(str1.repeat(3));
+//
+    }
+}
